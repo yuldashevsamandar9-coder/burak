a6815618fd52dcccddeaaf89c988d5cbd60e1fea
Sam <yuldashevsamandar9@gmail.com>
2026-05-21 17:34:35 +0900

    feat:BRR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("salom dunyo");
+
+import moment from "moment";
+const currentTime = moment().format();
+console.log(currentTime);
+
+const person: string = "Sam";
+const count: number = 100;
