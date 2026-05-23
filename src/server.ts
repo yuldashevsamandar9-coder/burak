fc7294a54343b685c909e0fca4e091e1ac784359
Sam <yuldashevsamandar9@gmail.com>
2026-05-23 16:47:23 +0900

    feat; integration of enveronmental variables

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,8 @@ console.log(currentTime);
 const person: string = "Sam";
 const count: number = 100;
 console.log("==========");
+
+import dotenv from "dotenv";
+dotenv.config();
+console.log("PORT:", process.env.PORT);
+console.log("MONGO_URL:", process.env.MONGO_URL);
