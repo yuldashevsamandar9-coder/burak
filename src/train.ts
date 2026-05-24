aeae933166b7ee974f2424cf73de017fc275ac1b
Sam <yuldashevsamandar9@gmail.com>
2026-05-24 21:18:10 +0900

    feat: M task bajarildi

--- src/train.ts
+++ src/train.ts
@@ -13,3 +13,23 @@ function getReversed(str: string): string {
 
 const result = getReversed("Mening farishtam ");
 console.log(result);
+
+/*  M-TASK
+
+Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array 
+ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil 
+topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin. 
+MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1},
+{number: 2, square: 4}, {number: 3, square: 9}].
+
+@MITASK
+
+*/
+const list = [1, 2, 3, 6, 8, 9, 0];
+const newList = list.map((ele) => {
+  return {
+    numbers: ele,
+    kvadrat: ele * ele,
+  };
+});
+console.log("newList:", newList);
