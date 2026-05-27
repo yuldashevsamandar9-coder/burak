53a6e32a6b79025d18c6a9c6e5a916f21474ea24
Sam <yuldashevsamandar9@gmail.com>
2026-05-27 16:11:22 +0900

    fix: modify routing and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /**  1 - ENTRANSE  **/
 const app = express();
@@ -14,6 +15,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /**  4 - ROUTER **/
-app.use("/", router); // Middlewere Disgn pattern
+app.use("/admin", routerAdmin); // BSSR : EJS
+app.use("/", router); // Middlewere Disgn pattern SPA REACT loyihamiz uchun backend server tarzida ishlatarkanmiz
 
 export default app; // module export app
