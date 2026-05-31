a86cf621330c052ff07da137b2d21648e85e4af2
Sam <yuldashevsamandar9@gmail.com>
2026-05-31 10:01:57 +0900

    feat: create restaurant member get and post endpoints

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -5,7 +5,8 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send("Home.page ");
+    res.send("Home page ");
+    // send | json | rederict | end | render
   } catch (err) {
     console.log("Eroor go home", err);
   }
@@ -13,6 +14,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
   try {
+    console.log("getLogin");
     res.send("Login.page ");
   } catch (err) {
     console.log("Eroor go getLogin", err);
@@ -27,4 +29,21 @@ restaurantController.getSignup = (req: Request, res: Response) => {
   }
 };
 
+restaurantController.processLogin = (req: Request, res: Response) => {
+  try {
+    console.log("processLogin");
+    res.send("DONE");
+  } catch (err) {
+    console.log("Eroor go processLogin ", err);
+  }
+};
+
+restaurantController.processSignup = (req: Request, res: Response) => {
+  try {
+    console.log("processSignup");
+    res.send("DONE");
+  } catch (err) {
+    console.log("Eroor go processSignup ", err);
+  }
+};
 export default restaurantController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,10 +2,17 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
+/** Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
+routerAdmin
+  .get("/login", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
+routerAdmin
+  .get("/signup", restaurantController.getSignup)
+  .post("/signup", restaurantController.processSignup);
 
-routerAdmin.get("/login", restaurantController.getLogin);
+/** Product */
 
-routerAdmin.get("/signup", restaurantController.getSignup);
+/** User */
 
 export default routerAdmin;
