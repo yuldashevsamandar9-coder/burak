1a382a1552580354e3009faeaba4ce2ab1021533
Sam <yuldashevsamandar9@gmail.com>
2026-06-01 13:22:50 +0900

    feat: O - TASK bajarildi

--- src/train.ts
+++ src/train.ts
@@ -59,3 +59,22 @@ function palindromCheck1(word3: string) {
 }
 console.log(palindromCheck1("dad"));
 console.log(palindromCheck1("mom"));
+
+/** O-TASK
+
+Shunday function yozing, u har xil valuelardan iborat array qabul qilsin 
+va array ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin.
+ MASALAN: calculateSumOfNumbers([10, "10", {son: 10}, true, 35]) return 45.
+
+@MITASK */
+function calculateSumOfNumbers(arr: any[]): number {
+  let count: number = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      count = count + arr[i];
+    }
+  }
+  return count;
+}
+
+console.log(calculateSumOfNumbers([7, "10", { son1: 5 }, true, 3]));
