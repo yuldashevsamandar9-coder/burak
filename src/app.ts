3c12c0b925395311a1f145ee2b8da4ef26754915
Sam <yuldashevsamandar9@gmail.com>
2026-06-07 09:50:11 +0900

    feat: integrate middelwere sessions

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,35 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+import ConnectMongoDBSession from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
 /**  1 - ENTRANSE  **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // Tradional Api
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
+
 /**  2 - SESSION **/
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, // 3 hours
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 
 /**  3 - VIEWS **/
 app.set("views", path.join(__dirname, "views"));
