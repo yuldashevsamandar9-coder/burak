48f7b39af0af4d51585372641a927033d20f88a8
Sam <yuldashevsamandar9@gmail.com>
2026-06-10 14:09:11 +0900

    feat: Product service modify intgrations

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,10 +1,10 @@
-import ProductModel from "../schema/Product.modul"
+import ProductModel from "../schema/Product.modul";
 
 class ProductService {
-    private readonly productModel;
-    
-    constructor() {
-        this.productModel = ProductModel;
-    }
-},
+  private readonly productModel;
+
+  constructor() {
+    this.productModel = ProductModel;
+  }
+}
 export default ProductService;
