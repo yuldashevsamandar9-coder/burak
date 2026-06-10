bf8bd37fa6da101f42ba2a85d75cd9faab89343e
Sam <yuldashevsamandar9@gmail.com>
2026-06-10 11:15:36 +0900

    feat: create product controller and routers

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response } from "express";
+import { T } from "../libs/types/common";
+import { Member, MemberInput } from "../libs/types/member";
+import Errors from "../libs/Errors";
+import ProductService from "../models/Product.service";
+
+const productService = new ProductService();
+const productController: T = {};
+
+productController.getAllProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getAllProducts");
+
+    // TODO: TOKENS AUTHENTICATIONS
+
+    res.render("products");
+  } catch (err) {
+    console.log("Errors go Signup ", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else
+      res
+        .status(Errors.standard.code)
+        .json({ message: Errors.standard.message });
+    //res.json({});
+  }
+};
+
+productController.createNewProduct = async (req: Request, res: Response) => {
+  try {
+    console.log("createProduct");
+
+    // TODO: TOKENS AUTHENTICATIONS
+  } catch (err) {
+    console.log("Errors go createNew Product", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else
+      res
+        .status(Errors.standard.code)
+        .json({ message: Errors.standard.message });
+    //res.json({});
+  }
+};
+
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+  try {
+    console.log("updateChosenProduct");
+
+    // TODO: TOKENS AUTHENTICATIONS
+  } catch (err) {
+    console.log("Errors go updateChosenProductcreateProduct", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else
+      res
+        .status(Errors.standard.code)
+        .json({ message: Errors.standard.message });
+    //res.json({});
+  }
+};
+
+export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 
 /** Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
@@ -11,10 +12,12 @@ routerAdmin
   .get("/signup", restaurantController.getSignup)
   .post("/signup", restaurantController.processSignup);
 routerAdmin.get("/logout", restaurantController.logout);
-
 routerAdmin.get("/check-me", restaurantController.checkAuthsession);
 
 /** Product */
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id ", productController.updateChosenProduct);
 
 /** User */
 
