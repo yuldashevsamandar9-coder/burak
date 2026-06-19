56a911c65458af1478063459e2c59114c5b2994c
Sam <yuldashevsamandar9@gmail.com>
2026-06-19 11:52:15 +0900

    feat: signup frontend  validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,27 @@
 console.log("Signup frontend javascript file");
+
+$(function () {});
+
+function validateSignupFrom() {
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
+
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === ""
+  ) {
+    event.preventDefault();
+    alert("Please insert all required inputs!");
+    return false;
+  }
+
+  if (memberPassword !== confirmPassword) {
+    event.preventDefault();
+    alert("Password differs please check!");
+    return false;
+  }
+}
