0580d514cdc829c249ec0ab08d3c768eaacc0ccc
Sam <yuldashevsamandar9@gmail.com>
2026-06-19 12:55:00 +0900

    feat: signup procces finish

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,31 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden ");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      console.log("uploadFile:", uploadFile);
+      const validImageType = ["image/jpg", "image/png", "image/jpeg"];
+
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg and png");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
 
 function validateSignupFrom() {
   const memberNick = $(".member-nick").val();
@@ -22,6 +47,14 @@ function validateSignupFrom() {
   if (memberPassword !== confirmPassword) {
     event.preventDefault();
     alert("Password differs please check!");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert("Please insert restaurant image");
     return false;
   }
 }
