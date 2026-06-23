23e40af2df81f1f6556da6219ad4780e340ffe04
Sam <yuldashevsamandar9@gmail.com>
2026-06-24 00:08:04 +0900

    feat: cretae new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -14,7 +14,7 @@ productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
     const data = await productService.getAllProducts();
-    console.log("products:", data);
+    // console.log("products:", data);
 
     res.render("products", { products: data });
   } catch (err) {
@@ -40,14 +40,14 @@ productController.createNewProduct = async (
 
     await productService.createNewProduct(data);
     res.send(
-      `<script> alert("Successfully creation"); window.location.replace('admin/product/all') </script>`,
+      `<script> alert("Successfully creation"); window.location.replace('/admin/product/all') </script>`,
     );
   } catch (err) {
     console.log("Error, createNewProduct:", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window.location.replace('admin/product/all') </script>`,
+      `<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`,
     );
   }
 };
