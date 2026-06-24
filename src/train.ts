d72214fc0f638a7383e942f1fd9e7e581dabff57
Sam <yuldashevsamandar9@gmail.com>
2026-06-24 13:29:10 +0900

    feat: W - TASK bajarildi

--- src/train.ts
+++ src/train.ts
@@ -216,3 +216,23 @@ function countChars(str: string) {
 }
 console.log(countChars("hello"));
 console.log(countChars("samandar"));
+
+/**
+ * W-TASK
+
+Shunday function yozing, uni array va number parametrlari bolsin. 
+Function arrayni numberda berilgan uzunlikda kesib bolaklarga ajratilgan array holatida qaytarsin.
+ MASALAN: chunkArray([1,2,3,4,5,6,7,8,9,10], 3) return [[1,2,3], [4,5,6], [7,8,9], [10]].
+
+@MITASK
+ */
+function chunkArray(arr: any[], size: number) {
+  let result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6));
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
